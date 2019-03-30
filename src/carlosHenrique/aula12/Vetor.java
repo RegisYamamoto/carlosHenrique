f97e7091026733e69a061c9ae02c118d53dd8fca
RegisYamamoto <regisyamamoto@gmail.com>
2019-03-30 08:18:14 -0300

    aula12

---
+++ src/carlosHenrique/aula12/Vetor.java
@@ -0,0 +1,18 @@
+package carlosHenrique.aula12;
+
+public class Vetor {
+
+	public static void main(String[] args) {
+		
+		/*String[] nomes = new String[3];
+		nomes[0] = "Regis Yamamoto";
+		nomes[1] = "Mariane Amaro";
+		nomes[2] = "Ana Amancio";*/
+		
+		String[] nomes = {"Regis Yamamoto", "Mariane Amaro", "Ana Amancio"};
+		
+		System.out.println(nomes[2]);
+		
+	}
+
+}
